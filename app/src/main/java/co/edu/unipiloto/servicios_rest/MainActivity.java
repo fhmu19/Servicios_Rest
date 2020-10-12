2ae0b3aa17317407bde1d8bf54ff55c7766971df
Fernando Martinez <fernandomartinezurrego@gmail.com>
2020-10-12 10:57:53 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/servicios_rest/MainActivity.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.servicios_rest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
