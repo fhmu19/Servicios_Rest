717a9ae5e18292dfaeb0aed3d93a9b156868ad00
Fernando Martinez <fernandomartinezurrego@gmail.com>
2020-10-12 12:55:39 -0500

    Commit2

--- app/src/main/java/co/edu/unipiloto/servicios_rest/MainActivity.java
+++ app/src/main/java/co/edu/unipiloto/servicios_rest/MainActivity.java
@@ -3,11 +3,54 @@ package co.edu.unipiloto.servicios_rest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+    private ListView lista;
+    private ArrayList<String> titles=new ArrayList<>();
+    private ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, titles);
+        lista=findViewById(R.id.list);
+        lista.setAdapter(arrayAdapter);
+        getPosts();
+    }
+    private void getPosts() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        PostService postService=retrofit.create(PostService.class);
+        Call<List<Post>> call=postService.getPost();
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                for (Post post : response.body()) {
+                    titles.add(post.getTitle());
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+
+            }
+        });
     }
 }
